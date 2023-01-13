df9d84ff23942ab710c0b086c596941789c7b06d
Youssef Soultan <youssefsoultan5@gmail.com>
2023-01-13 22:46:24 +0200

    v2

--- ANN and CNN.py
+++ ANN and CNN.py
@@ -3,6 +3,7 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold
 import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
 import os
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -195,7 +196,87 @@ def CNN_model(X_train, X_test, Y_train, Y_test):
     print("Mean AUC score: ", np.mean(auc))
 
 
-dir = '/Dataset/'
+def data_augmentation(X_train, X_test, Y_train, Y_test):
+    generator = ImageDataGenerator(
+        rotation_range=16,
+        width_shift_range=0.12,
+        height_shift_range=0.12,
+        zoom_range=0.12
+    )
+    generator.fit(X_train)
+    model3 = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(filters=64, kernel_size=(4, 4), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=2),
+        tf.keras.layers.Conv2D(filters=64, kernel_size=(4, 4), activation='relu'),
+        tf.keras.layers.MaxPooling2D(pool_size=2),
+        tf.keras.layers.Dropout(0.2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(10, activation='softmax')
+
+    ])
+    # Compile the model
+    model3.compile(optimizer=tf.keras.optimizers.Adam(), loss='categorical_crossentropy',
+                   metrics=['accuracy', tf.keras.metrics.Precision(), tf.keras.metrics.Recall(),
+                            tf.keras.metrics.AUC()])
+
+    # Create a KFold object with 5 folds
+    kfold = KFold(n_splits=5)
+
+    # Initialize lists to store the results for each fold
+    train_acc = []
+    val_acc = []
+    test_acc = []
+
+    train_loss = []
+    val_loss = []
+    test_loss = []
+
+    precision = []
+
+    recall = []
+
+    auc = []
+
+    # Loop through the folds
+    for train_index, test_index in kfold.split(X_train):
+        # Split the data into training and validation sets
+        X_train_fold, X_val_fold = X_train[train_index], X_train[test_index]
+        Y_train_fold, Y_val_fold = Y_train[train_index], Y_train[test_index]
+
+        # Fit the model on the training data
+        model3.fit(generator.flow(X_train_fold, Y_train_fold, batch_size=32), epochs=2, verbose=0)
+        # Extract the training, validation and testing accuracy from the evaluate method
+        fold_val_loss, fold_val_acc, _, _, _ = model3.evaluate(X_val_fold, Y_val_fold, verbose=0)
+        fold_train_loss, fold_train_acc, _, _, _ = model3.evaluate(X_train_fold, Y_train_fold, verbose=0)
+        fold_test_loss, fold_test_acc, fold_test_pre, fold_test_rec, fold_test_auc = model3.evaluate(X_test, Y_test,
+                                                                                                     verbose=0)
+
+        # Append the results to the lists
+        train_acc.append(fold_train_acc)
+        val_acc.append(fold_val_acc)
+        test_acc.append(fold_test_acc)
+
+        train_loss.append(fold_train_loss)
+        val_loss.append(fold_val_loss)
+        test_loss.append(fold_test_loss)
+
+        precision.append(fold_test_pre)
+
+        recall.append(fold_test_rec)
+
+        auc.append(fold_test_auc)
+    # Calculate the mean of the cross-validation scores
+
+    print("Mean Loss and Accuracy for training: ", [np.mean(train_loss), np.mean(train_acc)])
+    print("Mean Loss and Accuracy for validation: ", [np.mean(val_loss), np.mean(val_acc)])
+    print("Mean Loss and Accuracy for testing: ", [np.mean(test_loss), np.mean(test_acc)])
+    print("Mean Precision score: ", np.mean(precision))
+    print("Mean Recall score: ", np.mean(recall))
+    print("Mean AUC score: ", np.mean(auc))
+
+
+dir = '/Users/youssefsoultan/Desktop/Images/Dataset/'
 
 zero = dir + "/0"
 one = dir + "/1"
@@ -219,13 +300,15 @@ new_size = resize(64, 64, images)
 gray_scale = scale(1, new_size)
 rgb_scale = scale(0, new_size)
 
-X_gray = np.divide(gray_scale, 255)
+# Normalization
+X_gray = (gray_scale - gray_scale.mean()) / 255
 X_gray = np.array(X_gray).astype('float32')
 
 Y_gray = np.array(label).astype('float32')
 Y_gray = tf.keras.utils.to_categorical(label, 10)
 
-X_rgb = np.divide(rgb_scale, 255)
+# Normalization
+X_rgb = (rgb_scale - rgb_scale.mean()) / 255
 X_rgb = np.array(X_rgb).astype('float32')
 
 Y_rgb = np.array(label).astype('float32')
@@ -235,7 +318,7 @@ Y_rgb = tf.keras.utils.to_categorical(label, 10)
 X_train_gray, X_test_gray, Y_train_gray, Y_test_gray = train_test_split(X_gray, Y_gray, test_size=0.2)
 X_train_rgb, X_test_rgb, Y_train_rgb, Y_test_rgb = train_test_split(X_rgb, Y_rgb, test_size=0.2)
 
-print("Total number of gray images = " , X_gray.shape[0])
+print("Total number of gray images = ", X_gray.shape[0])
 print("number of training examples for gray images = ", X_train_gray.shape[0])
 print("number of test examples for gray images = ", X_test_gray.shape[0])
 print("X_train shape: ", X_train_gray.shape)
@@ -252,8 +335,11 @@ print("Y_train shape: ", Y_train_rgb.shape)
 print("X_test shape: ", X_test_rgb.shape)
 print("Y_test shape: ", Y_test_rgb.shape)
 
-print("ANN Results: ")
+print("ANN Model Results: ")
 ANN_model(X_train_gray, X_test_gray, Y_train_gray, Y_test_gray)
 print('<==================================================================>')
-print("CNN Results: ")
+print("CNN Model Results: ")
 CNN_model(X_train_rgb, X_test_rgb, Y_train_rgb, Y_test_rgb)
+print('<==================================================================>')
+print("CNN Model with Data Augmentation Results: ")
+data_augmentation(X_train_rgb, X_test_rgb, Y_train_rgb, Y_test_rgb)
